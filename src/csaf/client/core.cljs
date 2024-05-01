0361c1d6112d7519a3f5c8dfd0fbf30d5561734d
James N. V. Cash <james.cash@occasionallycogent.com>
2024-05-01 16:17:29 -0400

    Nav bar (non-working), image gallery

--- src/csaf/client/core.cljs
+++ src/csaf/client/core.cljs
@@ -6,14 +6,30 @@
 
 (defn app-view
   []
-  [:div {:tw "bg-white min-h-100vh w-80vw max-w-1000px mx-auto"}
+  [:div {:tw "bg-white min-h-100vh w-80vw max-w-1000px mx-auto font-sans-serif"}
    [:div.top-bar {:tw "flex flex-row items-center"}
-    [:img {:src "/images/csaf_logo.jpg" :alt "CSAF Logo"
-           :tw "w-10rem"}]
-    [:div {:tw "flex-grow"}
-     [:h1 {:tw "font-urbanist text-gray-500"}
-      "Canadian " [:span {:tw "text-black"} "Scottish Athletic"] " Federation"]]]
-   [:h1 "Hello, CSAF!"]])
+    [:a {:href "/"}
+     [:img {:src "/images/csaf_logo.jpg" :alt "CSAF Logo"
+            :tw "w-10rem"}]]
+    [:div {:tw "flex-grow flex flex-col"}
+     [:h1 {:tw "font-urbanist text-gray-500 text-center"}
+      "Canadian " [:span {:tw "text-black"} "Scottish Athletic"] " Federation"]
+     (let [a-tw "inline-block text-center hover:text-red-800 no-underline text-black h-2.5rem"]
+       [:nav {:tw "flex flex-col md:grid grid-cols-6 text-xs"}
+        [:a {:tw a-tw :href "/"} "Home"]
+        [:a {:tw a-tw :href "/"} "Athletes"]
+        [:a {:tw a-tw :href "/"} "Competitions"]
+        [:a {:tw a-tw :href "/"} "Information"]
+        [:a {:tw a-tw :href "/"} "About CSAF"]
+        [:a {:tw a-tw :href "/"} "Contact Us"]])]]
+   [:div.main {:tw "grid gap-2 mx-1"
+               :style {:grid-template-columns "15% 1fr 6rem"}}
+    [:div.gallery-image {:tw "h-100vh bg-no-repeat"
+                         :style {:animation "changeGalleryImage 30s infinite"}}]
+    [:div [:h1 "Hello, CSAF!"]]
+
+    [:div "COMING EVENTS"]]
+   ])
 
 (defn render
   []
--- src/csaf/client/styles.cljc
+++ src/csaf/client/styles.cljc
@@ -1,4 +1,6 @@
-(ns csaf.client.styles)
+(ns csaf.client.styles
+  (:require
+   [garden.stylesheet]))
 
 (defn styles []
   [
@@ -7,11 +9,27 @@
      :src [["url(\"/fonts/Urbanist-VariableFont_wght.ttf\")" "format(\"opentype\")"]]
      :font-style "normal"}]
 
+   (garden.stylesheet/at-keyframes
+     "changeGalleryImage"
+     [[" 0%" {:background-image "url(\"/images/gallery/image_0.jpg\")"}]
+      ["10%" {:background-image "url(\"/images/gallery/image_1.jpg\")"}]
+      ["20%" {:background-image "url(\"/images/gallery/image_2.jpg\")"}]
+      ["30%" {:background-image "url(\"/images/gallery/image_3.jpg\")"}]
+      ["40%" {:background-image "url(\"/images/gallery/image_4.jpg\")"}]
+      ["50%" {:background-image "url(\"/images/gallery/image_5.jpg\")"}]
+      ["60%" {:background-image "url(\"/images/gallery/image_6.jpg\")"}]
+      ["70%" {:background-image "url(\"/images/gallery/image_7.jpg\")"}]
+      ["80%" {:background-image "url(\"/images/gallery/image_8.jpg\")"}]
+      ["90%" {:background-image "url(\"/images/gallery/image_9.jpg\")"}]])
+
    [:body
     {:background-image "url(/images/celtic_knot_bg.jpg)"}]
 
    [:.font-urbanist
     {:font-family "'Urbanist', sans-serif"
      :font-weight "200"}]
+
+   [:.font-sans-serif
+    {:font-family "sans-serif"}]
 
    ])
