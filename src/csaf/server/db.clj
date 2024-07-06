097988254a0a2d3cd8171c677a2a4adec2860673
James N. V. Cash <james.cash@occasionallycogent.com>
2024-07-06 06:53:41 -0400

    Calculating rankings
    Something is still off -- open stone for juiniors and women's masters
    gives the wrong result -- but "close enough"

--- src/csaf/server/db.clj
+++ src/csaf/server/db.clj
@@ -1,6 +1,7 @@
 (ns csaf.server.db
   (:require
    [clojure.java.io :as io]
+   [clojure.math :as math]
    [com.rpl.specter :as x]
    [next.jdbc :as jdbc]
    [next.jdbc.prepare :as jdbc.prepare]
@@ -237,21 +238,27 @@
     ["select distinct extract(year from \"date\") as year
       from game_instances order by year desc"]))
 
+(declare event-records-for-year-by-class)
+(declare event-top-weights-for-year-by-class)
+(declare score-for-result)
+
 (defn games-history
   [{:keys [year classes event]}]
-  (->>
-    (jdbc/plan
-      @datasource
-      (cond->
-          [(str
-             "select games.id as games_id,
+  (let [bests (event-records-for-year-by-class year)
+        weights (event-top-weights-for-year-by-class year)]
+    (->>
+           (jdbc/plan
+             @datasource
+             (cond->
+                 [(str
+                    "select games.id as games_id,
          games.name,
          to_char(game_instances.date, 'YYYY-MM-DD') as date,
          game_results_placing.placing,
          members.id, members.first_name, members.last_name,
          game_member_results.event, game_member_results.distance_inches,
             game_member_results.clock_minutes, game_member_results.weight,
-            game_member_results.class
+            game_member_results.class, game_member_results.score
       from game_instances
       join games on games.id = game_instances.game_id
       join game_member_results on game_member_results.game_instance = game_instances.id
@@ -259,40 +266,51 @@
       join members on game_member_results.member_id = members.id
       where true
       "
-             (when year
-               " and extract(year from \"date\") = ? ")
-             (when (seq classes)
-               " and game_member_results.class = any(cast(? as membership_class_code[]))")
-             (when event
-               " and game_member_results.event = cast(? as game_event_type)"))]
-        (some? year) (conj year)
-        (seq classes) (conj (into-array java.lang.String classes))
-        (some? event) (conj event))
-      jdbc/snake-kebab-opts)
-    (reduce
-      (fn [acc row]
-        (cond-> acc
-          (not (contains? (get acc (:date row)) (:games_id row)))
-          (assoc-in [(:date row) (:games_id row)]
-                    {:games/name (:name row) :results {}})
+                    (when year
+                      " and extract(year from \"date\") = ? ")
+                    (when (seq classes)
+                      " and game_member_results.class = any(cast(? as membership_class_code[]))")
+                    (when event
+                      " and game_member_results.event = cast(? as game_event_type)"))]
+                 (some? year) (conj year)
+                 (seq classes) (conj (into-array java.lang.String classes))
+                 (some? event) (conj event))
+             jdbc/snake-kebab-opts)
+           (reduce
+             (fn [acc row]
+               (cond-> acc
+                 (not (contains? (get acc (:date row)) (:games_id row)))
+                 (assoc-in [(:date row) (:games_id row)]
+                           {:games/name (:name row) :results {}})
 
-          (not (contains? (get-in acc [(:date row) (:games_id row) :results])
-                          (:id row)))
-          (assoc-in [(:date row) (:games_id row) :results (:id row)]
-                    {:members/id (:id row)
-                     :members/first-name (:first_name row)
-                     :members/last-name (:last_name row)
-                     :game-results-placing/placing (:placing row)
-                     :game-member-results/class (:class row)
-                     :events {}})
+                 (not (contains? (get-in acc [(:date row) (:games_id row) :results])
+                                 (:id row)))
+                 (assoc-in [(:date row) (:games_id row) :results (:id row)]
+                           {:members/id (:id row)
+                            :members/first-name (:first_name row)
+                            :members/last-name (:last_name row)
+                            :game-results-placing/placing (:placing row)
+                            :game-member-results/class (:class row)
+                            :events {}})
 
-          true
-          (assoc-in [(:date row) (:games_id row) :results (:id row) :events (:event row)]
-                    {:game-member-results/event (:event row)
-                     :game-member-results/class (:class row)
-                     :game-member-results/clock-minutes (:clock_minutes row)
-                     :game-member-results/distance-inches (:distance_inches row)
-                     :game-member-results/weight (:weight row)}))) {})))
+                 true
+                 (assoc-in [(:date row) (:games_id row) :results (:id row) :events (:event row)]
+                           {:game-member-results/event (:event row)
+                            :game-member-results/class (:class row)
+                            :game-member-results/score (:score row)
+                            :game-member-results/clock-minutes (:clock_minutes row)
+                            :game-member-results/distance-inches (:distance_inches row)
+                            :game-member-results/weight (:weight row)})
+
+                 true (update-in [(:date row) (:games_id row) :results (:id row) :events (:event row)]
+                                 (fn [result]
+                                   (assoc result :calculated-score
+                                          (score-for-result
+                                            (:class row)
+                                            result
+                                            bests
+                                            weights))))))
+             {}))))
 
 (comment
 
@@ -395,3 +413,226 @@
                 (update-in acc [(:class row) (:event row)]
                            (fnil conj []))))
          {})))
+
+
+(defn- event-records-for-year-by-class
+  [year]
+  (jdbc/execute!
+    @datasource
+    ["select distinct first_value(class) over wnd as class,
+        first_value(event) over wnd as event,
+        first_value(distance_inches) over wnd as distance_inches,
+        first_value(weight) over wnd as weight,
+        first_value(year) over wnd as year
+      from event_records
+      where status = 'verified'
+      window wnd as (partition by class, event, weight order by
+         (case when year < ? then year else -year end) desc
+        rows between unbounded preceding and unbounded following)
+      order by distance_inches desc"
+     year]
+    jdbc/snake-kebab-opts))
+
+(defn- event-top-weights-for-year-by-class
+  [year]
+  (jdbc/execute!
+    @datasource
+    ["select distinct first_value(class) over wnd as class,
+        first_value(event) over wnd as event,
+        first_value(weight) over wnd as weight,
+        first_value(year) over wnd as year
+      from event_records
+      where status = 'verified'
+      window wnd as (partition by class, event order by
+         (case when year < ? then year else -year end) desc, weight desc
+        rows between unbounded preceding and unbounded following) "
+     year]
+    jdbc/snake-kebab-opts))
+
+(comment
+  (event-records-for-year-by-class 2023)
+  (event-records-for-year-by-class 1960)
+  )
+
+(def event-weight-limits
+  {"open" {"open" 16
+           "amateurs" 16
+           "masters" 16
+           "juniors" 12
+           "womens" 8
+           "womensmaster" 8
+           "lightweight" 16}})
+
+(defn score-for-result
+  ([class {:keys [year result]}]
+   (score-for-result
+     class
+     result
+     (event-records-for-year-by-class year)
+     (event-top-weights-for-year-by-class year)))
+  ([class {:game-member-results/keys [event weight distance-inches clock-minutes]}
+    event-records event-top-weights]
+   (let [class (case class
+                 "womensmaster" "womens"
+                 "amateurs" "open"
+                 class)]
+     (cond
+       (or (nil? distance-inches) (nil? weight)
+           (and (= "caber" event) (nil? clock-minutes))
+           (zero? distance-inches) (zero? weight))
+       0
+
+       (= "caber" event)
+       (+ (* 500 (/ (float weight) 160))
+          (* 500 (/ (float distance-inches) 258))
+          (- (/ (abs (float clock-minutes)) 2)))
+
+       (and (= "open" class) (= "open" event))
+       (let [light-best (x/select-first
+                          [x/ALL
+                           (x/if-path [:class (x/pred= class)] x/STAY)
+                           (x/if-path [:event (x/pred= event)] x/STAY)
+                           (x/if-path [:weight (x/view float) (x/pred= 17.0)] x/STAY)]
+                          event-records)
+             heavy-best (x/select-first
+                          [x/ALL
+                           (x/if-path [:class (x/pred= class)] x/STAY)
+                           (x/if-path [:event (x/pred= event)] x/STAY)
+                           (x/if-path [:weight (x/view float) (x/pred= 20.0)] x/STAY)]
+                          event-records)]
+         (if (and light-best heavy-best
+                  weight (<= (get-in event-weight-limits [event class]) (float weight)))
+           (let [factor (/ (- (float (:distance-inches light-best))
+                              (float (:distance-inches heavy-best)))
+                           (- 20 17))]
+             (-> (/ (+ (float distance-inches) (* (- factor 1) (- weight 17)))
+                    (float (:distance-inches light-best)))
+                 (* 1000)))
+           0))
+
+       (and (not= "masters" class) (= "open" event))
+       (let [best (x/select-first
+                    [x/ALL
+                     (x/if-path [:class (x/pred= class)] x/STAY)
+                     (x/if-path [:event (x/pred= event)] x/STAY)]
+                    event-records)
+             top-weight (x/select-one
+                          [x/ALL
+                           (x/if-path [:class (x/pred= class)] x/STAY)
+                           (x/if-path [:event (x/pred= event)] x/STAY)
+                           :weight]
+                          event-top-weights)
+             class-weight-limit (get-in event-weight-limits [event class])]
+         (if (and best top-weight class-weight-limit)
+           ;; this formula is bizarre, but shrug
+           (* 1000 (/ (+ (* 12 (+ (math/floor (/ (float distance-inches) 12))
+                                  (float weight)
+                                  (- class-weight-limit)))
+                         (mod (float distance-inches) 12))
+                      (+ (* 12 (+ (math/floor (/ (float (:distance-inches best)) 12))
+                                  (float top-weight)
+                                  (- class-weight-limit)))
+                         (mod (float (:distance-inches best)) 12))))
+           0))
+
+       :else
+       (-> (/ (float distance-inches)
+              (or (some-> (x/select-first
+                            [x/ALL
+                             (x/if-path [:class (x/pred= class)] x/STAY)
+                             (x/if-path [:event (x/pred= event)]
+                                        x/STAY)
+                             :distance-inches]
+                            event-records)
+                          float)
+                  ##Inf))
+           (* 1000)
+           (max 0))))))
+
+(comment
+  (jdbc/execute!
+    @datasource
+    ["select * from game_member_results where event = 'caber' and score <> 0 limit 1"]
+    jdbc/snake-kebab-opts)
+
+
+  (jdbc/execute! @datasource ["select * from game_instances where id = 639"])
+
+  (score-for-result
+    "womens"
+    {:result #:game-member-results{:member-id 175, :distance-inches 226.0M, :weight 18.00M, :class "womens", :score 812.9500M, :game-instance 831, :clock-minutes nil, :event "braemar", :id 33}
+     :year 2005})
+
+  (score-for-result
+    "open"
+    {:result #:game-member-results{:distance-inches 200M :weight 18M :class "open"
+                                   :event "open"}})
+
+  (score-for-result
+    "womens"
+    {:result #:game-member-results{:member-id 410, :distance-inches 192.0M, :weight 50.00M, :class "womens", :score 528.3430M, :game-instance 639, :clock-minutes 0, :event "caber", :id 20899}
+     :year 2009})
+
+(jdbc/execute!
+  @datasource
+  ["select * from game_member_results where event = 'caber' and score <> 0 limit 1"]
+  jdbc/snake-kebab-opts)
+  )
+
+;; Rankings
+
+(defn rankings-for-year
+  [year]
+  (->>
+    (jdbc/plan
+      @datasource
+      ["select distinct
+        last_value(game_member_results.class) over wnd as class,
+        last_value(game_member_results.score) over wnd as score,
+        last_value(game_member_results.event) over wnd as event,
+        last_value(game_member_results.member_id) over wnd as member_id,
+        last_value(game_member_results.distance_inches) over wnd as distance_inches,
+        last_value(game_member_results.clock_minutes) over wnd as clock_minutes,
+        last_value(game_member_results.weight) over wnd as weight,
+        last_value(members.first_name) over wnd as first_name,
+        last_value(members.last_name) over wnd as last_name
+     from game_member_results
+     join members on game_member_results.member_id = members.id
+     join game_instances on game_member_results.game_instance = game_instances.id
+     where extract(\"year\" from game_instances.\"date\") = ?
+       and members.country = any(ARRAY['Canada', ''])
+       and members.status = 'active'
+       and game_member_results.event <> 'sheaf'
+       and game_member_results.event <> 'braemar'
+       and game_member_results.score > 0
+       and game_member_results.class = any('{juniors, amateurs, womens, masters, open, lightweight}')
+     window wnd as (partition by game_member_results.member_id, game_member_results.event
+        order by score rows between unbounded preceding and unbounded following)"
+       year])
+    (reduce
+      (fn [acc row]
+        (let [cls (:class row)
+              cls (if (= cls "amateurs") "open" cls)]
+          ;; for the open class, get the best masters result & re-score it as open, use if better
+          ;; for the masters class, if the athlete is masters age, get best open result, calculate as masters, use if better
+          (-> acc
+              (assoc-in [cls (:member_id row) :members/first-name] (:first_name row))
+              (assoc-in [cls (:member_id row) :members/last-name] (:last_name row))
+              (assoc-in [cls (:member_id row) :events (:event row)]
+                        {:distance-inches (:distance_inches row)
+                         :weight (:weight row)
+                         :clock-minutes (:clock_minutes row)
+                         :score (:score row)})
+              (update-in [cls (:member_id row) :score]
+                         (fnil + 0) (:score row)))))
+      {})))
+
+(comment
+
+  (first (rankings-for-year 2023))
+  (get-in (rankings-for-year 2023)
+          ["open" 958])
+
+  (prn (map :score (vals (get-in (rankings-for-year 2023)
+                      [:open 958 :events]))))
+  )
