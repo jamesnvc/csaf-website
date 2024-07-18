31f9b9d42a7212c2af9c6662327f697fa5af6968
James N. V. Cash <james.cash@occasionallycogent.com>
2024-07-18 14:59:33 -0400

    WIP: ranking query

--- src/csaf/server/db.clj
+++ src/csaf/server/db.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.java.io :as io]
    [clojure.math :as math]
+   [clojure.string :as string]
    [com.rpl.specter :as x]
    [next.jdbc :as jdbc]
    [next.jdbc.prepare :as jdbc.prepare]
@@ -227,6 +228,124 @@
   (-> (member-pr-results 6)
       (get "caber") :game-instances/date class)
 
+  )
+
+(defn maybe-colliding-members
+  []
+  (jdbc/execute!
+    @datasource
+    ["select a.id as a_id, a.first_name as a_first, a.last_name as a_last,
+             b.id as b_id, b.first_name as b_first, b.last_name as b_last
+         from members a, members b
+      where a.id < b.id
+       and (a.first_name = b.first_name or starts_with(a.first_name, b.first_name)
+             or starts_with(b.first_name, a.first_name))
+       and a.last_name = b.last_name
+      order by a_last
+"]))
+
+(comment
+  (clojure.pprint/pprint (maybe-colliding-members))
+  (maybe-colliding-members)
+  [#:members{:a_id 64, :a_first "Dale", :a_last "Andrew", :b_id 1442, :b_first "D", :b_last "Andrew"}
+   #:members{:a_id 1127, :a_first "Benjamin", :a_last "Arthur", :b_id 1225, :b_first "Benjamin", :b_last "Arthur"}
+   #:members{:a_id 80, :a_first "Lance", :a_last "Barusch", :b_id 612, :b_first "L", :b_last "Barusch"}
+   #:members{:a_id 373, :a_first "Paul", :a_last "Boundy", :b_id 782, :b_first "P", :b_last "Boundy"}
+   #:members{:a_id 372, :a_first "Heather", :a_last "Boundy", :b_id 820, :b_first "H", :b_last "Boundy"}
+   #:members{:a_id 1099, :a_first "Russell", :a_last "Campbell", :b_id 1163, :b_first "Russ", :b_last "Campbell"}
+   #:members{:a_id 213, :a_first "Gord", :a_last "Charters", :b_id 385, :b_first "Gordon", :b_last "Charters"}
+   #:members{:a_id 58, :a_first "Steve", :a_last "Clark", :b_id 1266, :b_first "Steven", :b_last "Clark"}
+   #:members{:a_id 1426, :a_first "K", :a_last "Clark", :b_id 1602, :b_first "Katilyn", :b_last "Clark"}
+   #:members{:a_id 1266, :a_first "Steven", :a_last "Clark", :b_id 1278, :b_first "S", :b_last "Clark"}
+   #:members{:a_id 58, :a_first "Steve", :a_last "Clark", :b_id 1278, :b_first "S", :b_last "Clark"}
+   #:members{:a_id 1160, :a_first "Kaitlyn", :a_last "Clark", :b_id 1426, :b_first "K", :b_last "Clark"}
+   #:members{:a_id 189, :a_first "Tiffany", :a_last "DiRico", :b_id 705, :b_first "T", :b_last "DiRico"}
+   #:members{:a_id 52, :a_first "Adam", :a_last "Drummond", :b_id 920, :b_first "A", :b_last "Drummond"}
+   #:members{:a_id 114, :a_first "Danny", :a_last "Frame", :b_id 1480, :b_first "D", :b_last "Frame"}
+   #:members{:a_id 1561, :a_first "Alison", :a_last "Gaul", :b_id 1577, :b_first "Ali", :b_last "Gaul"}
+   #:members{:a_id 433, :a_first "Joe", :a_last "Hall", :b_id 1045, :b_first "Joe", :b_last "Hall"}
+   #:members{:a_id 1095, :a_first "Karl", :a_last "Hren", :b_id 1315, :b_first "K", :b_last "Hren"}
+   #:members{:a_id 841, :a_first "Robert", :a_last "Kingswood", :b_id 1189, :b_first "Rob", :b_last "Kingswood"}
+   #:members{:a_id 82, :a_first "Susie", :a_last "Lajoie", :b_id 1407, :b_first "S", :b_last "Lajoie"}
+   #:members{:a_id 131, :a_first "Norm", :a_last "Little", :b_id 1344, :b_first "N", :b_last "Little"}
+   #:members{:a_id 48, :a_first "Natasha", :a_last "Little", :b_id 1344, :b_first "N", :b_last "Little"}
+   #:members{:a_id 1, :a_first "George", :a_last "Loney", :b_id 1044, :b_first "George", :b_last "Loney"}
+   #:members{:a_id 1, :a_first "George", :a_last "Loney", :b_id 5, :b_first "G", :b_last "Loney"}
+   #:members{:a_id 5, :a_first "G", :a_last "Loney", :b_id 1044, :b_first "George", :b_last "Loney"}
+   #:members{:a_id 1379, :a_first "Erin", :a_last "Marte", :b_id 1528, :b_first "E", :b_last "Marte"}
+   #:members{:a_id 469, :a_first "Andre", :a_last "Mazerolle", :b_id 819, :b_first "A", :b_last "Mazerolle"}
+   #:members{:a_id 1586, :a_first "Alex ", :a_last "McCormick", :b_id 1587, :b_first "Alex ", :b_last "McCormick"}
+   #:members{:a_id 1319, :a_first "Alex", :a_last "McCormick", :b_id 1587, :b_first "Alex ", :b_last "McCormick"}
+   #:members{:a_id 1319, :a_first "Alex", :a_last "McCormick", :b_id 1586, :b_first "Alex ", :b_last "McCormick"}
+   #:members{:a_id 994, :a_first "Jason", :a_last "McDonald", :b_id 1428, :b_first "J", :b_last "McDonald"}
+   #:members{:a_id 1122, :a_first "Unknown", :a_last "Missing", :b_id 1217, :b_first "Unknown", :b_last "Missing"}
+   #:members{:a_id 1050, :a_first "Rob", :a_last "Moody", :b_id 1105, :b_first "Robert", :b_last "Moody"}
+   #:members{:a_id 1234, :a_first "Douglas", :a_last "Murphy", :b_id 1312, :b_first "Doug", :b_last "Murphy"}
+   #:members{:a_id 918, :a_first "Erinn", :a_last "Quinn", :b_id 1441, :b_first "E", :b_last "Quinn"}
+   #:members{:a_id 789, :a_first "Chris", :a_last "Racknor", :b_id 1254, :b_first "C", :b_last "Racknor"}
+   #:members{:a_id 1019, :a_first "Matthew", :a_last "Reilly", :b_id 1288, :b_first "Matt", :b_last "Reilly"}
+   #:members{:a_id 1092, :a_first "Matt", :a_last "Reling", :b_id 1110, :b_first "M", :b_last "Reling"}
+   #:members{:a_id 1460, :a_first "Nicholas", :a_last "Renyard", :b_id 1557, :b_first "Nic", :b_last "Renyard"}
+   #:members{:a_id 16, :a_first "Ray", :a_last "Siochowicz", :b_id 1177, :b_first "Ray", :b_last "Siochowicz"}
+   #:members{:a_id 511, :a_first "Gord", :a_last "Stalker", :b_id 1093, :b_first "Gordon", :b_last "Stalker"}
+   #:members{:a_id 1308, :a_first "Pamela", :a_last "Staples", :b_id 1380, :b_first "Pam", :b_last "Staples"}
+   #:members{:a_id 13, :a_first "Joel", :a_last "Thiessen", :b_id 990, :b_first "J", :b_last "Thiessen"}
+   #:members{:a_id 1250, :a_first "Alisha", :a_last "Thompson", :b_id 1473, :b_first "A", :b_last "Thompson"}
+   #:members{:a_id 630, :a_first "Jesse", :a_last "Trask", :b_id 1279, :b_first "J", :b_last "Trask"}
+   #:members{:a_id 629, :a_first "Jamie", :a_last "Trask", :b_id 1279, :b_first "J", :b_last "Trask"}
+   #:members{:a_id 614, :a_first "Owen", :a_last "Willems", :b_id 1352, :b_first "O", :b_last "Willems"}
+   #:members{:a_id 12, :a_first "Rob", :a_last "Young", :b_id 1600, :b_first "Robert", :b_last "Young"}
+   #:members{:a_id 12, :a_first "Rob", :a_last "Young", :b_id 551, :b_first "R", :b_last "Young"}
+   #:members{:a_id 551, :a_first "R", :a_last "Young", :b_id 1600, :b_first "Robert", :b_last "Young"}
+   #:members{:a_id 551, :a_first "R", :a_last "Young", :b_id 1020, :b_first "Richard", :b_last "Young"}
+   ]
+  )
+
+(defn- ->pg-ints
+  [coll]
+  (doto (PGobject.)
+    (.setType "int4[]")
+    (.setValue (str "{" (string/join "," coll) "}"))))
+
+;; we have Rob Young, Rob Young, and Robert Young
+(defn merge-members!
+  [[primary-member-id & other-member-ids]]
+  (jdbc/execute!
+    @datasource
+    ["begin;
+      update game_member_results set member_id = ?
+        where member_id = any(ARRAY[?]);
+      update game_results_placing set member_id = ?
+        where member_id = any(ARRAY[?]);
+      commit;"
+     primary-member-id (->pg-ints other-member-ids)
+     primary-member-id (->pg-ints other-member-ids)
+     ]))
+
+(comment
+  (let [robs [12 #_551 1600]]
+    (jdbc/execute!
+      @datasource
+      ["select id, member_id from game_member_results
+        where member_id = any(ARRAY[?])"
+       (doto (PGobject.)
+         (.setType "int4[]")
+         (.setValue (str "{" (string/join "," (rest robs)) "}")))]
+      ))
+  ;; "other rob" results
+  [#:game_member_results{:id 143663, :member_id 1600} #:game_member_results{:id 143661, :member_id 1600} #:game_member_results{:id 143662, :member_id 1600} #:game_member_results{:id 143660, :member_id 1600} #:game_member_results{:id 143658, :member_id 1600} #:game_member_results{:id 143659, :member_id 1600} #:game_member_results{:id 143656, :member_id 1600} #:game_member_results{:id 143657, :member_id 1600}]
+  (let [robs [12 551 1600]]
+    (jdbc/execute!
+      @datasource
+      ["select game_instance_id, member_id from game_results_placing
+        where member_id = any(ARRAY[?])"
+       (doto (PGobject.)
+         (.setType "int4[]")
+         (.setValue (str "{" (string/join "," (rest robs)) "}")))]
+      ))
+  [#:game_results_placing{:game_instance_id 1653, :member_id 1600}]
+
+  (merge-members! [12 #_551 1600])
   )
 
 ;;; Games queries
@@ -510,7 +629,7 @@
             (* -1/2 (+ (* 16 (math/floor (/ (float clock-minutes) 60)))
                        (mod (float clock-minutes) 60)))))
 
-       #_#_(and weight (< (float weight)
+       (and weight (< (float weight)
                       (get-in event-weight-limits [event class] ##Inf)))
        0
 
@@ -638,12 +757,52 @@
 
 ;; Rankings
 
+(defn supplemental-results-for-masters
+  [member-id]
+  (->>
+    (jdbc/execute!
+      @datasource
+      ["select game_member_results.class, score, event, member_id,
+               distance_inches, clock_minutes, game_member_results.weight
+        from game_member_results
+        join game_instances on game_instance = game_instances.id
+        join members on game_member_results.member_id = members.id
+        where member_id = ?
+          and game_instances.date >=
+             coalesce(members.master_first_date, members.birth_date + '40 years'::interval)
+          and members.country = any(ARRAY['Canada', ''])
+          and members.status = 'active'
+          and game_member_results.event <> 'sheaf'
+          and game_member_results.event <> 'braemar'
+          and game_member_results.score > 0
+          and game_member_results.class = any('{juniors, amateurs, womens, masters, open, lightweight}')"
+        member-id])))
+
 (defn rankings-for-year
   [year]
-  (->>
-    (jdbc/plan
-      @datasource
-      ["select distinct
+  (let [bests (event-records-for-year-by-class year)
+        weights (event-top-weights-for-year-by-class year)
+        process-row
+        (fn [acc cls row]
+          (-> acc
+              (assoc-in [cls (:member_id row) :members/id] (:member_id row))
+              (assoc-in [cls (:member_id row) :members/first-name] (:first_name row))
+              (assoc-in [cls (:member_id row) :members/last-name] (:last_name row))
+              (cond->
+                  (< (get-in acc [cls (:member_id row) :events (:event row) :score] 0)
+                     (:score row))
+                (->
+                  (assoc-in [cls (:member_id row) :events (:event row)]
+                            {:distance-inches (:distance_inches row)
+                             :weight (:weight row)
+                             :clock-minutes (:clock_minutes row)
+                             :score (:score row)})
+                  (update-in [cls (:member_id row) :score]
+                             (fnil + 0) (:score row))))))]
+    (->>
+      (jdbc/plan
+        @datasource
+        ["(select distinct
         last_value(game_member_results.class) over wnd as class,
         last_value(game_member_results.score) over wnd as score,
         last_value(game_member_results.event) over wnd as event,
@@ -652,7 +811,9 @@
         last_value(game_member_results.clock_minutes) over wnd as clock_minutes,
         last_value(game_member_results.weight) over wnd as weight,
         last_value(members.first_name) over wnd as first_name,
-        last_value(members.last_name) over wnd as last_name
+        last_value(members.last_name) over wnd as last_name,
+        (extract(\"year\" from members.birth_date + '40 years'::interval) <= ? or
+            extract(\"year\" from members.master_first_date) <= ?) as masters_age
      from game_member_results
      join members on game_member_results.member_id = members.id
      join game_instances on game_member_results.game_instance = game_instances.id
@@ -664,29 +825,83 @@
        and game_member_results.score > 0
        and game_member_results.class = any('{juniors, amateurs, womens, masters, open, lightweight}')
      window wnd as (partition by game_member_results.member_id, game_member_results.event
-        order by score rows between unbounded preceding and unbounded following)"
-       year])
-    (reduce
-      (fn [acc row]
-        (let [cls (:class row)
-              cls (if (= cls "amateurs") "open" cls)]
-          ;; for the open class, get the best masters result & re-score it as open, use if better
-          ;; for the masters class, if the athlete is masters age, get best open result, calculate as masters, use if better
-          (-> acc
-              (assoc-in [cls (:member_id row) :members/first-name] (:first_name row))
-              (assoc-in [cls (:member_id row) :members/last-name] (:last_name row))
-              (assoc-in [cls (:member_id row) :events (:event row)]
-                        {:distance-inches (:distance_inches row)
-                         :weight (:weight row)
-                         :clock-minutes (:clock_minutes row)
-                         :score (:score row)})
-              (update-in [cls (:member_id row) :score]
-                         (fnil + 0) (:score row)))))
-      {})))
+        order by score rows between unbounded preceding and unbounded following)
+      ) union all (
+       -- get open results for masters that have just reached 40 this year
+       select distinct
+        last_value(game_member_results.class) over wnd as class,
+        last_value(game_member_results.score) over wnd as score,
+        last_value(game_member_results.event) over wnd as event,
+        last_value(game_member_results.member_id) over wnd as member_id,
+        last_value(game_member_results.distance_inches) over wnd as distance_inches,
+        last_value(game_member_results.clock_minutes) over wnd as clock_minutes,
+        last_value(game_member_results.weight) over wnd as weight,
+        last_value(members.first_name) over wnd as first_name,
+        last_value(members.last_name) over wnd as last_name,
+        true as masters_age
+     from game_member_results
+     join members on game_member_results.member_id = members.id
+     join game_instances on game_member_results.game_instance = game_instances.id
+     where (extract(\"year\" from members.birth_date + '40 years'::interval) = ? or
+            extract(\"year\" from members.master_first_date) = ?)
+       and game_instances.date >= coalesce(members.birth_date + '40 years'::interval,
+                                           members.master_first_date)
+       and members.country = any(ARRAY['Canada', ''])
+       and members.status = 'active'
+       and game_member_results.event <> 'sheaf'
+       and game_member_results.event <> 'braemar'
+       and game_member_results.score > 0
+       and game_member_results.class = 'open'
+     window wnd as (partition by game_member_results.member_id, game_member_results.event
+        order by score rows between unbounded preceding and unbounded following))"
+         year year year year year])
+      (reduce
+        (fn [acc row]
+          (let [cls (:class row)
+                cls (if (= cls "amateurs") "open" cls)]
+            (cond-> (process-row acc cls row)
+
+              ;; for the masters class, if the athlete is masters
+              ;; age, get best open result, calculate as masters,
+              ;; use if better
+              (and (= cls "open") (:masters_age row))
+              (process-row
+                "masters"
+                (assoc
+                  row :score
+                  (score-for-result
+                    "masters"
+                    #:game-member-results{:event (:event row)
+                                          :weight (:weight row)
+                                          :distance-inches (:distance_inches row)
+                                          :clock-minutes (:clock_minutes row)}
+                    bests weights)))
+
+              ;; for the open class, if athlete is also
+              ;; masters-age, get the best masters result
+              ;; & re-score it as open, use if better
+              ;; (but needs to be the correct weight?)
+              (= cls "masters")
+              (process-row
+                "open"
+                (assoc
+                  row :score
+                  (score-for-result
+                    "open"
+                    #:game-member-results{:event (:event row)
+                                          :weight (:weight row)
+                                          :distance-inches (:distance_inches row)
+                                          :clock-minutes (:clock_minutes row)}
+                    bests weights))))))
+        {}))))
 
 (comment
 
-  (first (rankings-for-year 2023))
+  (x/select
+    [(x/keypath "masters" 12)
+     ]
+    (rankings-for-year 2023))
+  (supplemental-results-for-masters 12)
   (get-in (rankings-for-year 2023)
           ["open" 958])
 
