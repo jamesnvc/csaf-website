32b6a44f2a3967a6d86bb9adf4c4ad9eb2af8196
James N. V. Cash <james.cash@occasionallycogent.com>
2024-08-06 11:57:53 -0400

    Whoops, fix name of jar

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git clean -fx -e config.edn
 git fetch
 git merge --ff
 lein uberjar
-JAR_NAME="cosmiccms-${NOW}.jar"
+JAR_NAME="csaf-${NOW}.jar"
 git tag -a "deploy-$NOW" -m "Deploying at ${NOW}"
 scp target/csaf-0.1.0-SNAPSHOT-standalone.jar csaf-new:~/csaf/$JAR_NAME
 ssh csaf-new "bash -c 'cd ~/csaf; ln -sf ${JAR_NAME} csaf.jar; date > deployed'"
